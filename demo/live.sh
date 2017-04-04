b50d50212edef04d41c906c92fd38c3fc46ab2a8
Ricardo Bocchi <ricardo@mobilemind.com.br>
2017-04-03 23:24:26 -0300

    add sh

---
+++ demo/live.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+
+# --log trace
+tns livesync android --device 0015127775 --watch 
---
+++ demo/run.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+#tns run android --emulator --geny Custom_Phone_-_4.2.2_-_API_17_-_768x1280
+
+
+tns run android --device 0015127775 --log trace #celular
+#tns run android --device C4F1264FC37368E #--log trace #tablet
